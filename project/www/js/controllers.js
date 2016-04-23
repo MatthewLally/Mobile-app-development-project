e92f4c1d7cf8ebe075d614cac6adf3684f2695af
Matthew <Matthew>
2016-04-23 11:43:45 +0100

    Made some changes and fixed errors

--- project/www/js/controllers.js
+++ project/www/js/controllers.js
@@ -6,27 +6,6 @@ angular.module('starter.controllers', [])
 
 .controller('PersonelTasksCtrl', function($scope , Task){
     $scope.data = Task.data;
-    $scope.data = {
-        showDelete: false
-    };
-    
-    $scope.edit = function(task){
-        alert('Edit Item: ' + task.id);
-    };
-    
-    $scope.share = function(item){
-        alert('Share Item : ' + task.id);
-    };
-    
-    $scope.moveTask = function(task , fromTask , toTask) {
-        $scope.tasks.splice(fromTask ,1);
-        $scope.tasks.splice(toTask , 0 , task);
-    };
-    
-    $scope.onItemDelete = 
-        function(item){
-        $scope.tasks.splice($scope.tasks.indexOf(task), 1);
-    };
 })
             
 .controller('addTaskCtrl', function($scope , Task , Task2){
