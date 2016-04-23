9f34d6ded815150aba36e597836ec214921bd528
Matthew <Matthew>
2016-04-23 14:27:11 +0100

    Got buttons working!

--- project/www/js/controllers.js
+++ project/www/js/controllers.js
@@ -2,10 +2,34 @@ angular.module('starter.controllers', [])
 
 .controller('WorkTasksCtrl', function($scope , Task2 ) {
     $scope.data2 = Task2.data2;
+    $scope.moveTask2 = function (task2 , fromIndex , toIndex)
+    {
+        //Move the item in the array
+        $scope.data2.tasks2.splice(fromIndex , 1);
+        $scope.data2.tasks2.splice(toIndex , 0 , task2)
+    }
+    
+    $scope.removeTask2 = function (index)
+    {
+        $scope.data2.tasks2.splice(index , 1);
+    }
 })
 
+//Adapted from http://ionicframework.com/docs/api/directive/ionReorderButton/
 .controller('PersonelTasksCtrl', function($scope , Task){
     $scope.data = Task.data;
+    $scope.moveTask = function(task , fromIndex , toIndex)
+    {
+        //Move the item in the array
+        $scope.data.tasks.splice(fromIndex , 1);
+        $scope.data.tasks.splice(toIndex , 0 , task)
+    }
+    
+    //Adapted from http://stackoverflow.com/questions/32214519/how-to-add-remove-item-to-from-list-in-ionic
+    $scope.removeTask = function (index)
+    {
+        $scope.data.tasks.splice(index , 1);
+    }
 })
 
 
